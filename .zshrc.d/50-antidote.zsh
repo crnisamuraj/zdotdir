9d393de0758c5148da1c7ca3ed2c353ce441682f
Vudu Ronin <m1ivancevic@gmail.com>
2026-06-25 21:55:17 +0200

    Handle missing Antidote sources

--- .zshrc.d/50-antidote.zsh
+++ .zshrc.d/50-antidote.zsh
@@ -2,10 +2,20 @@
 ZSH_AUTOSUGGEST_STRATEGY=(history)
 
 # Antidote - plugin manager
-if [[ -n $HOMEBREW_PREFIX && -d $HOMEBREW_PREFIX/opt/antidote/share/antidote ]]; then
-  source $HOMEBREW_PREFIX/opt/antidote/share/antidote/antidote.zsh
-elif [[ -d ${ZDOTDIR:-$HOME}/.antidote ]]; then
-  source ${ZDOTDIR:-$HOME}/.antidote/antidote.zsh
+if [[ -n $HOMEBREW_PREFIX && -r $HOMEBREW_PREFIX/opt/antidote/share/antidote/antidote.zsh ]]; then
+  source "$HOMEBREW_PREFIX/opt/antidote/share/antidote/antidote.zsh"
+elif [[ -r /usr/share/zsh-antidote/antidote.zsh ]]; then
+  source /usr/share/zsh-antidote/antidote.zsh
+elif [[ -r /usr/share/antidote/antidote.zsh ]]; then
+  source /usr/share/antidote/antidote.zsh
+elif [[ -r ${ZDOTDIR:-$HOME}/.antidote/antidote.zsh ]]; then
+  source "${ZDOTDIR:-$HOME}/.antidote/antidote.zsh"
+else
+  print -u2 "Error: antidote.zsh was not found; skipping zsh plugin loading."
 fi
 
-antidote load ${ZDOTDIR:-$HOME}/.zsh_plugins.txt
+if (( $+functions[antidote] )); then
+  antidote load "${ZDOTDIR:-$HOME}/.zsh_plugins.txt"
+else
+  print -u2 "Error: antidote was not sourced properly; skipping zsh plugin loading."
+fi
